81c4e2b26a1cbf42c4f3890495e668221243b6c6
Stephen Lakowske <stevel95gm@gmail.com>
2014-02-08 08:43:13 -0800

    Initial commit

---
+++ RobLog/RobLog/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RobLog
+//
+//  Created by Stephen Lakowske on 2/8/14.
+//  Copyright (c) 2014 Cal Poly Hackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ RobLog/RobLogTests/RobLogTests.m
@@ -0,0 +1,34 @@
+//
+//  RobLogTests.m
+//  RobLogTests
+//
+//  Created by Stephen Lakowske on 2/8/14.
+//  Copyright (c) 2014 Cal Poly Hackathon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RobLogTests : XCTestCase
+
+@end
+
+@implementation RobLogTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
